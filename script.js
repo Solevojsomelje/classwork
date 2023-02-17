ece4372b3fa09fea9dff0cbfc23e828ce4aaf27e
Rostik <carinikrost@gmail.com>
2023-02-17 17:51:06 +0200

    First test commit

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+// alert("hello");
+// const result = confirm("Are you here");
+// console.log(result);
+// const answer = +prompt("Вам есть 18?", "22");
+// console.log(typeof answer);
+const answers = [];
+answers[0] = prompt("Как ваше имя?", "");
+answers[1] = prompt("Как ваше фамилия?", "");
+answers[2] = prompt("Сколько вам лет?", "");
+
+console.log(typeof answers);
+console.log(typeof null); //Возвращает объект, признаная ошибка
